d4d58f02095677eab62065d5b3d82fc56cceefe4
Pedro Baselga <91091320+PedBaSa@users.noreply.github.com>
2026-05-25 21:48:21 +0200

    Update RegisterController.java

--- src/mapademo/RegisterController.java
+++ src/mapademo/RegisterController.java
@@ -99,42 +99,43 @@ public class RegisterController implements Initializable {
         email_err.setText("");
         birth_err.setText("");
         
-        String password = passwordReg.getText();
-        String username = userReg.getText();
-        String emailUser = email.getText();
-        LocalDate birthD = birthDate.getValue();
+        String password=passwordReg.getText();
+        String username=userReg.getText();
+        String emailUser=email.getText();
+        LocalDate birthD=birthDate.getValue();
         
-        if(password.isEmpty() || username.isEmpty() || emailUser.isEmpty() || birthD == null){
+        if(password.isEmpty()||username.isEmpty()||emailUser.isEmpty()||birthD==null){
             ErrorReg.setText("Please fill all mandatory fields");
             return;
         }
-
+        
         boolean hasError = false;
         
         if(!User.checkNickName(username)){
-            user_err.setText("Nick must be 6-15 characters, letters, digits, hyphen or underscore only");
+            user_err.setText("Nick must be 6-15 characters ,letters ,digits ,hyphen or underscore only");
             hasError = true;
         }
         
         if(!User.checkPassword(password)){
-            pass_err.setText("8 to 20 characters, with at least one uppercase & lowercase letter, one digit and one symbol");
+            pass_err.setText(" 8 to 20 characters, with at least one uppercase & lowercase letter, one digit and one symbol");
             hasError = true;
         }
         
         if(!User.checkEmail(emailUser)){
-            email_err.setText("Use valid user@domain format");
+            email_err.setText(" valid user@domain format");
             hasError = true;
         }
         
-        if(!User.isOlderThan(birthD, 12)){
-            birth_err.setText("The user must be at least 12 years old");
+        if(!User.isOlderThan(birthD,12)){
+            birth_err.setText("the user must be atleast 12 years old");
             hasError = true;
         }
         
-        if (hasError) return;
-
-        SportActivityApp app = SportActivityApp.getInstance();
-        boolean reg = app.registerUser(username, emailUser, password, birthD, rutaAvatar);
+        if (hasError) return; // STOP EXECUTION IF DATA IS INVALID
+        
+        SportActivityApp app=SportActivityApp.getInstance();
+         
+        boolean reg=app.registerUser(username, emailUser, password, birthD, rutaAvatar);
          
         if(reg){
             ErrorReg.setText("Successful operation");
@@ -144,10 +145,11 @@ public class RegisterController implements Initializable {
                 Scene scene = new Scene(mapaRoot);
                 stage.setScene(scene);
                 stage.show();
-            } catch(IOException e){
-                System.out.println("Error while loading main screen");
+            }catch(IOException e){
+                System.out.println("Error while laoding main screen");
+                e.printStackTrace();
             }
-        } else {
+        }else{
             ErrorReg.setText("Problem with registration");
         }
      }
